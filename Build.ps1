d7fb0a36dfa709a04659591b043c72e55db0656b
rupertbenbrook <rupert@altitudeangel.com>
2022-11-18 15:05:57 +0000

    Update SOPS decryption

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,10 @@
+Write-Host "Creating cluster..."
 kind create cluster --image kindest/node:v1.24.7@sha256:577c630ce8e509131eab1aea12c022190978dd2f745aac5eb1fe65c0807eb315
+Write-Host "Installing flux..."
 flux install
+Write-Host "Creating SOPS secret..."
+$key = Read-Host -Prompt "SOPS Private Key" -AsSecureString
+kubectl create secret generic sops-gpg --namespace=flux-system --from-literal=sops.asc=$(ConvertFrom-SecureString $key)
+Write-Host "Bootstrapping cluster..."
 flux bootstrap github --owner=rupertbenbrook --repository=fluxtest --path=clusters/my-cluster --personal
+Write-Host "Done"
