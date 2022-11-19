7d4164aa2d0f7152605cb6879fe4619154fd8304
rupertbenbrook <rupert@altitudeangel.com>
2022-11-19 12:15:28 +0000

    Updated build

--- Build.ps1
+++ Build.ps1
@@ -3,8 +3,8 @@ kind create cluster --image kindest/node:v1.24.7@sha256:577c630ce8e509131eab1aea
 Write-Host "Installing flux..."
 flux install
 Write-Host "Creating SOPS secret..."
-$key = Read-Host -Prompt "SOPS Private Key" -AsSecureString
-kubectl create secret generic sops-gpg --namespace=flux-system --from-literal=sops.asc=$(ConvertFrom-SecureString $key -AsPlainText)
+$key = Read-Host -Prompt "SOPS AGE Private Key" -AsSecureString
+kubectl create secret generic sops-age --namespace=flux-system --from-literal=age.agekey=$(ConvertFrom-SecureString $key -AsPlainText)
 Write-Host "Bootstrapping cluster..."
 flux bootstrap github --owner=rupertbenbrook --repository=fluxtest --path=clusters/my-cluster --personal
 Write-Host "Done"
